4b945cc68977f2065270c09ca1a6ed189a7d0cea
jbreslin <94449521+jbreslin7@users.noreply.github.com>
2022-11-25 11:29:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+## CSV files in python
+# csv files are easy to make 
+# very readable to humans
+
+##Question 2 input 5 numbers 
+# code = input("please type your 5 digit code(please add commas): ")
+# print (code)
+# list(code)
+# list.sort(code)
+##Question 3
+## abstraction
+# abstraction is the 
+
+# binary = int(01001010+10011001)
+# print(binary)
+
+##Question 4
+# def is_anagram(word1, word2):
+#    if sorted(word1) == sorted(word2):
+#     return True
+#    else:
+#     return False
+
+# word1 = input("Enter the first word: ")
+# word2 = input("Enter the second word: ")
+
+
+# if (sorted(word1) == (sorted(word2))):
+#   print(word1,"is an anagram of" ,word2)
+
+
+# elif (sorted(word1) != (sorted(word2))):
+#    print(word1,"is not an anagram of" ,word2)
+
+# is_anagram(word1, word2)
+# print(is_anagram)
